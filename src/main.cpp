0f356a57a4e1c8445c01652368c758618c2dd9cd
Nathan <nathanpykimutai@gmail.com>
2020-11-29 16:35:52 +0300

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+String api = "http://192.168.0.107:5000/api/v1";
+
+void setup() {
+  //baud rate
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
